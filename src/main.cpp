259a00beb44a79c3a3ab0108b031d7c7627b8ebd
chaitanya mandala <nagachaitanya948@gmail.com>
2025-09-11 00:49:54 +0530

    added all hardware working functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include <Adafruit_VL53L0X.h>
+#include <Adafruit_NeoPixel.h>
+#include <ESP32Servo.h>
 
 // Motor driver pin definitions
 #define LEFT_MOTOR_IN1 25  // ESP32 GPIO25 -> MX1508 IN1
@@ -11,6 +13,15 @@
 // ToF sensor object
 Adafruit_VL53L0X tof = Adafruit_VL53L0X();
 
+// NeoPixel LED setup
+#define NEOPIXEL_PIN 16 // ESP32 GPIO2 for NeoPixel data
+#define NUM_PIXELS 1    // Number of LEDs (just 1)
+Adafruit_NeoPixel pixels(NUM_PIXELS, NEOPIXEL_PIN, NEO_GRB + NEO_KHZ800);
+
+// Servo setup
+#define SERVO_PIN 17 // ESP32 GPIO17 for servo control
+Servo servo;
+
 // Function declarations
 void setupMotors();
 void setLeftMotor(int power);
@@ -19,6 +30,10 @@ void setBothMotors(int leftPower, int rightPower);
 void stopMotors();
 void setupToF();
 float getDistance();
+void setupNeoPixel();
+void setPixelColor(uint8_t r, uint8_t g, uint8_t b);
+void setupServo();
+void setServoAngle(int angle);
 
 void setup()
 {
@@ -27,6 +42,8 @@ void setup()
 
   setupMotors();
   setupToF();
+  setupNeoPixel();
+  setupServo();
 
   Serial.println("Setup complete. Starting main loop...");
   delay(2000);
@@ -34,8 +51,6 @@ void setup()
 
 void loop()
 {
-
-  delay(100); // Read distance every second
 }
 
 // Initialize motor pins
@@ -63,17 +78,17 @@ void setLeftMotor(int power)
 
   if (power > 0)
   {
-    // Forward direction
-    analogWrite(LEFT_MOTOR_IN1, power);
-    digitalWrite(LEFT_MOTOR_IN2, LOW);
+    // Forward direction (inverted)
+    digitalWrite(LEFT_MOTOR_IN1, LOW);
+    analogWrite(LEFT_MOTOR_IN2, power);
     Serial.print("Left Motor: Forward, Power: ");
     Serial.println(power);
   }
   else if (power < 0)
   {
-    // Reverse direction
-    digitalWrite(LEFT_MOTOR_IN1, LOW);
-    analogWrite(LEFT_MOTOR_IN2, abs(power));
+    // Reverse direction (inverted)
+    analogWrite(LEFT_MOTOR_IN1, abs(power));
+    digitalWrite(LEFT_MOTOR_IN2, LOW);
     Serial.print("Left Motor: Reverse, Power: ");
     Serial.println(abs(power));
   }
@@ -94,17 +109,17 @@ void setRightMotor(int power)
 
   if (power > 0)
   {
-    // Forward direction
-    analogWrite(RIGHT_MOTOR_IN3, power);
-    digitalWrite(RIGHT_MOTOR_IN4, LOW);
+    // Forward direction (inverted)
+    digitalWrite(RIGHT_MOTOR_IN3, LOW);
+    analogWrite(RIGHT_MOTOR_IN4, power);
     Serial.print("Right Motor: Forward, Power: ");
     Serial.println(power);
   }
   else if (power < 0)
   {
-    // Reverse direction
-    digitalWrite(RIGHT_MOTOR_IN3, LOW);
-    analogWrite(RIGHT_MOTOR_IN4, abs(power));
+    // Reverse direction (inverted)
+    analogWrite(RIGHT_MOTOR_IN3, abs(power));
+    digitalWrite(RIGHT_MOTOR_IN4, LOW);
     Serial.print("Right Motor: Reverse, Power: ");
     Serial.println(abs(power));
   }
@@ -164,4 +179,47 @@ float getDistance()
   {
     return -1.0; // Error reading
   }
+}
+
+// Initialize NeoPixel LED
+void setupNeoPixel()
+{
+  pixels.begin();
+  pixels.clear();
+  pixels.show();
+  Serial.println("NeoPixel LED initialized");
+}
+
+// Set LED color with RGB values (0-255 each)
+void setPixelColor(uint8_t r, uint8_t g, uint8_t b)
+{
+  pixels.setPixelColor(0, pixels.Color(r, g, b));
+  pixels.show();
+  Serial.print("LED Color set to RGB(");
+  Serial.print(r);
+  Serial.print(", ");
+  Serial.print(g);
+  Serial.print(", ");
+  Serial.print(b);
+  Serial.println(")");
+}
+
+// Initialize servo
+void setupServo()
+{
+  servo.attach(SERVO_PIN);
+  servo.write(90); // Start at center position
+  Serial.println("Servo initialized at center position (90 degrees)");
+}
+
+// Set servo angle (0-180 degrees)
+void setServoAngle(int angle)
+{
+  // Constrain angle to valid range
+  angle = constrain(angle, 0, 180);
+
+  servo.write(angle);
+  Serial.print("Servo angle set to: ");
+  Serial.print(angle);
+  Serial.println(" degrees");
 }
